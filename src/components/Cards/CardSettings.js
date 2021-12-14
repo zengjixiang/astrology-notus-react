a94ce20a1e0ae284b40e26a5f76a9c7997375b5e
zengjixiang <zengjixiang0401@gmail.com>
2021-12-14 23:36:22 +0800

    修改設定頁面

--- src/components/Cards/CardSettings.js
+++ src/components/Cards/CardSettings.js
@@ -8,19 +8,19 @@ export default function CardSettings() {
       <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
         <div className="rounded-t bg-white mb-0 px-6 py-6">
           <div className="text-center flex justify-between">
-            <h6 className="text-blueGray-700 text-xl font-bold">My account</h6>
+            <h6 className="text-blueGray-700 text-xl font-bold">我的帳號</h6>
             <button
               className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
               type="button"
             >
-              Settings
+              儲存設定
             </button>
           </div>
         </div>
         <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
           <form>
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-              User Information
+              使用者資訊
             </h6>
             <div className="flex flex-wrap">
               <div className="w-full lg:w-6/12 px-4">
@@ -29,7 +29,7 @@ export default function CardSettings() {
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Username
+                    使用者名稱
                   </label>
                   <input
                     type="text"
@@ -44,7 +44,7 @@ export default function CardSettings() {
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Email address
+                    Email 帳號
                   </label>
                   <input
                     type="email"
@@ -59,7 +59,7 @@ export default function CardSettings() {
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    First Name
+                    名字
                   </label>
                   <input
                     type="text"
@@ -74,7 +74,7 @@ export default function CardSettings() {
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Last Name
+                    姓氏
                   </label>
                   <input
                     type="text"
@@ -88,7 +88,7 @@ export default function CardSettings() {
             <hr className="mt-6 border-b-1 border-blueGray-300" />
 
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-              Contact Information
+              聯絡資訊
             </h6>
             <div className="flex flex-wrap">
               <div className="w-full lg:w-12/12 px-4">
@@ -97,7 +97,7 @@ export default function CardSettings() {
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Address
+                    地址
                   </label>
                   <input
                     type="text"
@@ -112,7 +112,7 @@ export default function CardSettings() {
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    City
+                    城市
                   </label>
                   <input
                     type="email"
@@ -127,7 +127,7 @@ export default function CardSettings() {
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Country
+                    國家
                   </label>
                   <input
                     type="text"
@@ -142,7 +142,7 @@ export default function CardSettings() {
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Postal Code
+                    郵遞區號
                   </label>
                   <input
                     type="text"
@@ -156,7 +156,7 @@ export default function CardSettings() {
             <hr className="mt-6 border-b-1 border-blueGray-300" />
 
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-              About Me
+              關於我
             </h6>
             <div className="flex flex-wrap">
               <div className="w-full lg:w-12/12 px-4">
